44ba32a89226b2a2e7ae210d17b6e2af127f7964
tevel-ftc / FtcPowerPlay <tevel.ftc@gmail.com>
2025-12-07 18:17:48 +0200

    intake and transfer separated2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systemTeleops/TransferTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systemTeleops/TransferTeleop.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.systemTeleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.systems.Intake;
 import org.firstinspires.ftc.teamcode.systems.Transfer;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -11,7 +13,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class TransferTeleop extends LinearOpMode {
 
-    private Transfer intake;
+    private Transfer transfer;
     FtcDashboard dashboard;
     public static double up = 0.2;
 
@@ -20,7 +22,7 @@ public class TransferTeleop extends LinearOpMode {
     @Override
     public void runOpMode() {
         // Initialize the intake system
-        intake = new Transfer(this);
+        transfer = new Transfer(this);
 
 
         telemetry.addLine("Initialized — Ready to start");
@@ -32,12 +34,12 @@ public class TransferTeleop extends LinearOpMode {
         while (opModeIsActive()) {
             // If square (X) button is pressed, run intake
             if (gamepad2.triangle){
-                intake.setTransferPower(up);
+                transfer.setTransferPower(up);
             }
             if (gamepad2.cross){
-                intake.setTransferPower(down);
+                transfer.setTransferPower(down);
             }
-            telemetry.addData("Intake Power", intake.intakeMotor.getPower());
+            telemetry.addData("Intake Power", transfer.transferMotor.getPower());
             telemetry.update();
         }
     }

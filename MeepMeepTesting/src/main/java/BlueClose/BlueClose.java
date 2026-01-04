90cd0fef51dcfe6b502f7dd8d475db87d0ba74a8
tevel-ftc / FtcPowerPlay <tevel.ftc@gmail.com>
2026-01-04 16:41:04 +0200

    MeepMeepStarted

--- MeepMeepTesting/src/main/java/BlueClose/BlueClose.java
+++ MeepMeepTesting/src/main/java/BlueClose/BlueClose.java
@@ -1,11 +1,16 @@
-package com.example.meepmeeptesting;
+package BlueClose;
+
+import static java.lang.Math.PI;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+import MeepMeep.coordinates.BlueCloseCoordinatesMeepMeep;
+
+public class BlueClose {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(670);
 
@@ -14,15 +19,11 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(BlueCloseCoordinatesMeepMeep.getStart())
+                .setTangent(Math.toRadians(45))
+                .splineToLinearHeading(BlueCloseCoordinatesMeepMeep.getFirstIntakeStart(), BlueCloseCoordinatesMeepMeep.getFirstIntakeStart().heading)
+                .splineToSplineHeading(BlueCloseCoordinatesMeepMeep.getFirstIntakeEnd(), BlueCloseCoordinatesMeepMeep.getFirstIntakeEnd().heading)
+                        .splineToLinearHeading(BlueCloseCoordinatesMeepMeep.getShooting(), BlueCloseCoordinatesMeepMeep.getShooting().heading)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
--- MeepMeepTesting/src/main/java/MeepMeep/coordinates/BlueCloseCoordinatesMeepMeep.java
+++ MeepMeepTesting/src/main/java/MeepMeep/coordinates/BlueCloseCoordinatesMeepMeep.java
@@ -1,4 +1,51 @@
 package MeepMeep.coordinates;
 
+import static java.lang.Math.PI;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class BlueCloseCoordinatesMeepMeep {
+
+    private static final double startX = -50;
+    private static final double startY = -50;
+    private static final double startPoseHeading = Math.toRadians(234);
+
+    private static final double firstIntakeStartX = -12.5;
+    private static final double firstIntakeStartY = -35;
+    private static final double firstIntakeStartHeading = Math.toRadians(270);
+    private static final double firstIntakeSEndY = -50;
+
+
+    private static final double shootingX = -20;
+    private static final double shootingY = -15;
+    private static final double shootingHeading = Math.toRadians(225);
+
+
+
+    private static final Pose2d start = new Pose2d(startX, startY, startPoseHeading);
+    private static final Pose2d firstIntakeStart = new Pose2d(firstIntakeStartX, firstIntakeStartY, firstIntakeStartHeading);
+    private static final Pose2d firstIntakeEnd = new Pose2d(firstIntakeStartX, firstIntakeSEndY, firstIntakeStartHeading);
+
+
+
+    private static final Pose2d shooting = new Pose2d(shootingX, shootingY, shootingHeading);
+
+
+    public static Pose2d getStart() {
+        return start;
+    }
+
+    public static Pose2d getFirstIntakeStart() {
+        return firstIntakeStart;
+    }
+    public static Pose2d getFirstIntakeEnd() {
+        return firstIntakeEnd;
+    }
+    public static Pose2d getShooting(){
+        return shooting;
+    }
 }
